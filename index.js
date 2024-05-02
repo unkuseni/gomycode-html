c6ece3483b8d98485515bfb1573907cf0f0984c9
unkuseni <amudalatadeoye@gmail.com>
2024-05-02 05:17:25 +0100

    done with nav

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
-const mobileNavBtn = document.querySelector(".mobile-nav-btn");
 
 
-mobileNavBtn.addEventListener("click", () => {
-	const navList = document.querySelector(".nav-list");
-	navList.classList.toggle("active");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+const navLinks = document.querySelectorAll(".navlink");
+menuBtn.addEventListener("click", () => {
+	const navList = document.querySelector("#nav-list");
+	if (window.getComputedStyle(navList).getPropertyValue("display") == "none") {
+		navList.style.display = "flex";
+		navList.classList.add("open-nav");
+	} else {
+		navList.style.display = "none";
+		navList.classList.remove("open-nav");
+	}
+	console.log(window.getComputedStyle(navList).getPropertyValue("display"));
+});
+
+closeBtn.addEventListener("click", () => {
+	const navList = document.querySelector("#nav-list");
+	navList.style.display = "none";
+	navList.classList.remove("open-nav");
+});
+
+
+window.addEventListener("resize", () => {
+	const navList = document.querySelector("#nav-list");
+	let width = window.innerWidth;
+	if (width > 768) {
+		navList.style.display = "flex";
+		navList.classList.remove("open-nav");
+	} else {
+		navList.style.display = "none";
+		navList.classList.remove("open-nav");
+	}
+})
+
+
+navLinks.forEach(link => {
+	link.addEventListener("click", () => {
+		const navList = document.querySelector("#nav-list");
+		navList.style.display = "none";
+		navList.classList.remove("open-nav");
+	})
 })
